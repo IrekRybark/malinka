1cb7fb14aa4a5a43a16ceaf92058e4b306a26259
v01dma1n <irek@post.com>
2016-05-19 14:36:51 -0400

    MVC model for clock

---
+++ malinka/pwm/clockmvc.py
@@ -0,0 +1,78 @@
+import datetime
+import time
+
+from observable import Observable
+
+class ClockModel(object):
+    """
+    The class is responsible for handling time.
+    Specifically, it can retrieve time form the environment (OS), which is default or overridden to read from some
+    other source (like hardware clock)
+    """
+    def __init__(self):
+        self.time = Observable(datetime.time)
+
+    def update_time(self):
+        self.time.set(datetime.datetime.now())
+
+    def get_hour(self):
+        return self.time.hour
+
+    def get_minute(self):
+        return self.time.minute
+
+    def get_second(self):
+        return self.time.second
+
+
+class ClockView(object):
+    """
+    The class is responsible for presenting time.
+    The time can be shown in traditional way using clock face or some other method like binary numbers or voltmeters.
+    """
+
+    def set_time(self, time):
+        self.time = time
+        self.show()
+
+    def show(self):
+        print "Set time: ", self.time.hour, ":", self.time.minute, ":", self.time.second
+
+
+class ClockController(object):
+    """
+    The class is responsible for updating attached view(s) based on changing values from model.
+    """
+    def __init__(self):
+        self.model = self.make_model()
+        self.views = []
+        self.add_view(self.make_view())
+        self.model.time.add_callback(self._time_changed)
+
+    def make_model(self):
+        return ClockModel()
+
+    def make_view(self):
+        return ClockView()
+
+    def add_view(self, view):
+        self.views.append(view)
+
+    def del_view(self, view):
+        for v in self.views:
+            if v is view:
+                self.views.remove(v)
+
+    def _time_changed(self, time):
+        for v in self.views:
+            v.set_time(time)
+
+    def update_time(self):
+        self.model.update_time()
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
--- malinka/pwm/rpiclock.py
+++ malinka/pwm/rpiclock.py
@@ -1,70 +1,57 @@
 import datetime
 import time
 
-from observable import Observable
+from clockmvc import ClockController, ClockView
 
 
-class ClockModel():
-    """
-    The class is responsible for handling time.
-    Specifically, it can retrieve time form the environment (OS) or dedicated hardware.
-    """
-    def __init__(self, time):
-        self.time = Observable(time)
-
-    def update_time(self, time):
-        self.time.set(time)
-
-    def get_hour(self):
-        return self.time.hour
-
-    def get_minute(self):
-        return self.time.minute
-
-    def get_second(self):
-        return self.time.second
-
-
-class ClockView():
+class RPiClockViewMeters(ClockView):
     """
     The class is responsible for presenting time.
     The time can be shown in traditional way using clock face or some other method like binary numbers or voltmeters.
     """
+    def __init__(self):
+        super(RPiClockViewMeters, self).__init__()
+        self.view_name = 'RPi'
+
+        self.gpio_pin_h = 4
+        self.gpio_pin_m = 21
+        self.gpio_pin_s = 22
 
     def set_time(self, time):
         self.time = time
         self.show()
 
     def show(self):
-        print "Set time: ", self.time.hour, ":", self.time.minute, ":", self.time.second
+        print self.view_name
+        print "Hour meter:    ", self.time.hour
+        print "Minute meter:  ", self.time.minute
+        print "Second meters: ", self.time.second
+#        self.pi.set_PWM_range(GPIO_CLK_H, GPIO_CLK_RANGE)
+#        self.pi.set_PWM_range(GPIO_CLK_M, GPIO_CLK_RANGE)
+#        self.pi.set_PWM_range(GPIO_CLK_S, GPIO_CLK_RANGE)
 
 
-class ClockController():
-    """
-    The class is responsible for updating attached view(s) based on changing values from model.
-    """
-    def __init__(self):
-        self.model = ClockModel(datetime.datetime.now())
-        self.view = ClockView()
-        self.model.time.add_callback(self._time_changed)
+class RPiClockController(ClockController):
 
-    def _time_changed(self, time):
-        self.view.set_time(time)
-
-    def update_time(self, time):
-        self.model.update_time(time)
-
+    def make_view(self):
+        # super(RPiClockController, self).ma
+        return RPiClockViewMeters()
 
 def main():
-    controller = ClockController()
+    controller = RPiClockController()
     delay = 3
-    controller.update_time(datetime.datetime.now())
+    controller.update_time()
     time.sleep(delay)
-    controller.update_time(datetime.datetime.now())
+
+    new_view = RPiClockViewMeters()
+    controller.add_view(new_view)
+    new_view.view_name = 'RPi2'
+    controller.update_time()
+
     time.sleep(delay)
-    controller.update_time(datetime.datetime.now())
 
-
+    controller.del_view(new_view)
+    controller.update_time()
 
 
 if __name__ == "__main__":
