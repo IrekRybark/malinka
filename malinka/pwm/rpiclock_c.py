8345ea80b2769af9a8d9d9f341a5cb2b599843ea
Irek Rybark <irek@post.com>
2016-06-01 22:46:02 -0400

    testing chiming

--- malinka/pwm/rpiclock_c.py
+++ malinka/pwm/rpiclock_c.py
@@ -1,12 +1,15 @@
 from clockmvc import ClockController
 from config import Config
 from rpiclock_v_meters import RPiClockViewMeters
+from rpiclock_v_chimes import RPiClockViewChimes
+
 
 class RPiClockController(ClockController):
 
     def __init__(self):
         super(RPiClockController, self).__init__()
         self.add_view(RPiClockViewMeters())
+        self.add_view(RPiClockViewChimes())
 
 if __name__ == "__main__":
     pass
---
+++ malinka/pwm/rpiclock_v_chimes.py
@@ -0,0 +1,54 @@
+from pygame_sdl2 import mixer 
+
+from clockmvc import ClockView
+from config import Config
+
+
+class RPiClockViewChimes(ClockView):
+    """
+    The class is responsible for presenting time (hours, half-hours)
+    as sounds (chimes).
+    """
+    
+    def __init__(self):
+        super(RPiClockViewChimes, self).__init__()
+        self.view_name = 'RPiChimes'
+        
+        mixer.init()
+        
+        self.cfg = Config('clock.cfg')
+        self.sound_hour = mixer.Sound(self.cfg.chimes_hour_sound)
+        self.sound_halfhour = mixer.Sound(self.cfg.chimes_halfhour_sound)
+        
+        self.last_played_minute = None
+        
+        if mixer.get_num_channels() < 1:
+            raise Exception('mixer does not have any channels')
+        self.channel = mixer.Channel(0)
+        
+    def show(self):
+        """ """ 
+        # ToDo: fix the test intervals 
+        
+        if self.last_played_minute == None:
+            self.last_played_minute = self.time.minute  # do not make sound when starting up
+        if self.last_played_minute != self.time.minute:
+            self.last_played_minute = self.time.minute
+            
+            count = (self.time.minute % 10)  # 0...9
+            if count == 0:
+                count = 9  # 10 times
+            else:
+                count -= 1 # count + 1 times
+                
+            if self.time.minute % 2 == 0:
+                sound = self.sound_hour
+            else:
+                sound = self.sound_halfhour
+            
+            self.play_hour(sound, count) 
+
+    def play_hour(self, sound, hour=0):
+        
+        self.channel.play(sound, hour)
+
